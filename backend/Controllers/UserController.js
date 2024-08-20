f41cde48e1d30d8a3ad9093ecb32708a12d2a186
thasneem9 <135808068+thasneem9@users.noreply.github.com>
2024-08-20 19:39:30 +0530

    added bcrypt ,and logic for hashing password

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -1,19 +1,25 @@
 
 import User from "../Models/User.js";
+import bcrypt from 'bcrypt';
 
 const signup= async (req,res)=>{
 
 const dataReceived=req.body;
+const saltRounds=10;
 console.log(dataReceived)
 try {
+    //take password andhash it.
+    const password=dataReceived.password
+    const hashedPassword=await bcrypt.hash(password,saltRounds)
     //it is converntional in REST APIs, to sendback the same data fprm db once creatig/updating a column in table
     const newUser=await User.create({
         name:dataReceived.name,
         email:dataReceived.email,
         username:dataReceived.username,
-        password:dataReceived.password
+        password:hashedPassword
 
     })
+
     console.log(
         "newuser:",newUser)
     res.status(200).json({ message: "Signup successful",data:newUser });
